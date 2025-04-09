9d0dc8bc9eb98388d435c005280608525a44da07
Andres Rodriguez <andres_rod2013@hotmail.com>
2025-04-08 21:59:46 -0300

    creamos las urls en el proyecto

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -15,8 +15,44 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from historia import views
+
+
+
+
+
+
+
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+    
+   
+    
+    
 ]
+
+urlpatterns += [
+
+    path("categoria/list/", views.categoria_list, name="categoria_list"),
+    path("categoria/create/", views.categoria_create, name="categoria_create"),
+    path("categoria/update/<int:pk>", views.categoria_update, name="categoria_update"),
+   path("categoria/delete/<int:pk>/", views.categoria_delete, name="categoria_delete"),
+
+]
+
+urlpatterns += [
+    path("historia/list/", views.HistoriaListView.as_view(), name="historia_list"),
+    path("historia/create/", views.HistoriaCreateView.as_view(), name="historia_create"),
+    path("historia/update/<int:pk>", views.HistoriaUpdateView.as_view(), name="historia_update"),
+    path("historia/detail/<int:pk>", views.HistoriaDetailView.as_view(), name="historia_detail"),
+    path("historia/delete/<int:pk>/", views.HistoriaDeleteView.as_view(), name="historia_confirm_delete"),
+    path('historia/', include(('historia.urls', 'historia'), namespace='historia')),
+
+
+
+    ]
