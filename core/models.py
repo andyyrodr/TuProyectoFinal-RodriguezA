9d061214d42c9a0cd7cf34420c754fe1510a42d0
Andres Rodriguez <andres_rod2013@hotmail.com>
2025-04-08 21:52:38 -0300

    creamos los modelos en core

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from django.shortcuts import render 
+
+
+
+def index(request):
+    return render(request, "core/index.html")
+
+
+
+def about(request):
+    return render(request, "core/about.html")
 
